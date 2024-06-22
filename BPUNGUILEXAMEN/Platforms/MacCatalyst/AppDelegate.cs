bad819399590e16202ff29408ffe949a0582fc39
master <master@SMLAB304PC19_24>
2024-06-21 20:22:45 -0500

    Agregar archivos de proyecto.

---
+++ BPUNGUILEXAMEN/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace BPUNGUILEXAMEN
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
